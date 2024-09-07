cf95985a69eb3aa9c72dd1101b7bf6e659a92ffe
Ahmad Hasan <55281583+DevAhmad7@users.noreply.github.com>
2024-09-07 15:28:09 +0300

    updates noval v1.1.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,19 +4,19 @@ export as namespace noval;
 
 export declare function useSelector(
   selector: undefined | array | string | Function,
-  fallback: any
+  fallback?: any
 ): any;
 
 export declare function useDispatch(): {
   reset: (values: string | array) => undefined;
   dirty: (values: string | array) => undefined;
-  addState: (values: object, selector: string) => undefined;
-  dispatch: (param1: any, param2: any) => Function | Promise;
+  addState: (values: object, selector?: string) => undefined;
+  dispatch: (param1: any, param2?: any) => Function | Promise;
 };
 
 export declare function ProviderNoval(): ReactElement;
 
 export default function noval(
-  initialState: object,
-  createDispatch: Function
+  initialState?: object,
+  createDispatch?: Function
 ): typeof ProviderNoval;
